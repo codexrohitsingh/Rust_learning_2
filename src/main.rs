aba613a5e2a0c4d427dc22780f6e81f972f7a5ed
rohit singh <codex.rohit.singh@gmail.com>
2025-10-03 22:41:13 +0530

    Question 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// Write a functino is_even that takes a number as an imput and retuns true if it is even 
 fn main() {
-    println!("Hello, world!");
+    println!("{}",is_even(22));
+
+}
+
+fn is_even(a:i32)->bool{
+    if a%2==0
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
 }
